c2c8575a7358952059d975443574baf69abafe5b
simonholmes001 <simonholmesabc@gmail.com>
2020-04-06 08:17:29 +0200

    Combine amino acid tags & psi angles

---
+++ ramachandran/psi_angles.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import csv
+import pickle
+
+# Development of matching amino acid labels to psi angles
+psi_data_path = '../dihedral_coordinates'
+amino_acid_data_path = '../../structure_prediction/output/final_features'
+
+class PsiDihedralAngleStatistics:
+    def __init__(self, psi_data_path, amino_acid_data_path, name):
+        self.psi_data_path = psi_data_path
+        self.amino_acid_data_path = amino_acid_data_path
+        self.name = name
+
+    def get_amino_acid_array(self):
+        """Extracts the amino acid sequence from the _tag data"""
+        self.amino_acid_list = []
+        with open(self.amino_acid_data_path +'/' + self.name + '_amino_acid_tag_.csv', newline='') as f:
+            self.reader = csv.reader(f)
+            self.data = list(self.reader)
+            for i in self.data:
+                self.amino_acid_list.append(i[0])
+        return self.amino_acid_list
+
+    def encode(self):
+        self.alphabet = 'ALA','ARG','ASN','ASP','CYS','GLN','GLU','GLY','HIS','ILE','LEU','LYS','MET','PHE','PRO','SER','THR','TRP','TYR','VAL', 'SEC', 'PYL', 'UNK'
+        self.char_to_int = dict((c, i) for i, c in enumerate(self.alphabet))
+        # int_to_char = dict((i, c) for i, c in enumerate(alphabet))
+        self.amino_acid_encoded = [self.char_to_int[char] for char in self.amino_acid_list]
+        del self.amino_acid_encoded[-1]
+        return self.amino_acid_encoded
+
+    def get_psi(self):
+        with open(self.psi_data_path + '/' + self.name + '_psi.pickle', 'rb') as labels_file:
+            self.psi_df = pd.read_pickle(labels_file)
+            self.psi_array = np.array(self.psi_df)
+
+    def check_length(self):
+        print("Print len before: {}".format(len(self.amino_acid_encoded)))
+        print("Print len psi: {}".format(len(self.psi_array)))
+        delta = len(self.amino_acid_encoded) - len(self.psi_array)
+        if delta != 0:
+            del self.amino_acid_encoded[-delta+1]
+        print("Print len after: {}".format(len(self.amino_acid_encoded)))
+        print("Print len psi: {}".format(len(self.psi_array)))
+
+    def combine_amino_acid_psi(self):
+            self.amino_psi_array = np.column_stack([self.amino_acid_encoded, self.psi_array])
+            return self.amino_psi_array
+
+    def save_psi_angles(self):
+        with open(self.psi_data_path + '/' + self.name.split('_')[0] + '_psi_.pickle', 'wb') as file:
+            pickle.dump(self.amino_psi_array, file) # Save as a pickle object
