a6f11ab317eefd0683407bdc9e2aeb7e75a60913
simonholmes001 <simonholmesabc@gmail.com>
2020-04-07 08:48:24 +0200

    Add tests for phi angles

--- tests/test_ramachandran.py
+++ tests/test_ramachandran.py
@@ -8,7 +8,6 @@ import numpy as np
 import pandas as pd
 from ramachandran.ramachandran import Ramachandran
 
-
 class TestRamachandran(unittest.TestCase):
     """Tests for `ramachandran` package."""
 
@@ -65,7 +64,7 @@ class TestRamachandran(unittest.TestCase):
         test_sample = '2j5p_dihedral_matrix_.csv'
         test_df = pd.read_csv(test_sample, header=None)
         self.test_array = test_df[[2, 3, 4]].to_numpy() # numpy array containing only the atomic coordinate information
-        Ramachandran.vectorise(self)
+        Ramachandran.vectorise_psi(self)
         Ramachandran.psi(self)
 
         a = np.array([13.085,11.834,6.526])
@@ -112,7 +111,7 @@ class TestRamachandran(unittest.TestCase):
         cos_theta_3 = np.dot(normal_ghi, normal_hij) / (np.linalg.norm(normal_ghi) * np.linalg.norm(normal_hij))
         theta_test_3 = np.arccos(cos_theta_3)
 
-        for v in self.vectors[0:9]:
+        for v in self.vectors_psi[0:9]:
             print(v)
         print("Normal_abc: {}".format(normal_abc))
         print("Normal_bcd: {}".format(normal_bcd))
@@ -128,32 +127,32 @@ class TestRamachandran(unittest.TestCase):
         print("Theta_3: {}".format(theta_test_3))
 
         elements_0 = []
-        for i in self.vectors[0]:
+        for i in self.vectors_psi[0]:
             elements_0.append(i)
         for j in range(len(elements_0)):
             self.assertAlmostEqual(elements_0[j],normal_abc[j])
         elements_1 = []
-        for i in self.vectors[1]:
+        for i in self.vectors_psi[1]:
             elements_1.append(i)
         for j in range(len(elements_1)):
             self.assertAlmostEqual(elements_1[j],normal_bcd[j])
         elements_3 = []
-        for i in self.vectors[3]:
+        for i in self.vectors_psi[3]:
             elements_3.append(i)
         for j in range(len(elements_3)):
             self.assertAlmostEqual(elements_3[j],normal_def[j])
         elements_4 = []
-        for i in self.vectors[4]:
+        for i in self.vectors_psi[4]:
             elements_4.append(i)
         for j in range(len(elements_4)):
             self.assertAlmostEqual(elements_4[j],normal_efg[j])
         elements_6 = []
-        for i in self.vectors[6]:
+        for i in self.vectors_psi[6]:
             elements_6.append(i)
         for j in range(len(elements_6)):
             self.assertAlmostEqual(elements_6[j],normal_ghi[j])
         elements_7 = []
-        for i in self.vectors[7]:
+        for i in self.vectors_psi[7]:
             elements_7.append(i)
         for j in range(len(elements_7)):
             self.assertAlmostEqual(elements_7[j],normal_hij[j])
@@ -162,12 +161,114 @@ class TestRamachandran(unittest.TestCase):
         self.assertAlmostEqual(self.psi_angle[1], theta_test_2)
         self.assertAlmostEqual(self.psi_angle[2], theta_test_3)
 
+    def test_003_calculate_phi(self):
+        """"""
+        test_sample = '2j5p_dihedral_matrix_.csv'
+        test_df = pd.read_csv(test_sample, header=None)
+        self.test_array = test_df[[2, 3, 4]].to_numpy() # numpy array containing only the atomic coordinate information
+        Ramachandran.vectorise_phi(self)
+        Ramachandran.phi(self)
+
+        a = np.array([13.085,11.834,6.526])
+        b = np.array([13.538,10.454,6.211])
+        c = np.array([12.726,9.855,5.067])
+        d = np.array([12.964,8.579,4.784])
+        e = np.array([12.246,7.911,3.714])
+        f = np.array([12.416,6.405,3.752])
+        g = np.array([13.316,5.892,2.920])
+        h = np.array([13.573,4.458,2.859])
+        i = np.array([14.002,3.911,4.219])
+        j = np.array([14.502,4.793,5.080])
+        k = np.array([14.949,4.394,6.411])
+        l = np.array([13.843,3.666,7.168])
+        m = np.array([12.609,4.120,6.978])
+
+        # test calculation of vector normal to the plane containing the points c, d, e & f
+        cd = np.array([d[0]-c[0], d[1]-c[1], d[2]-c[2]])
+        ce = np.array([e[0]-c[0], e[1]-c[1], e[2]-c[2]])
+        normal_cde = np.cross(cd,ce)
+        de = np.array([e[0]-d[0], e[1]-d[1], e[2]-d[2]])
+        df = np.array([f[0]-d[0], f[1]-d[1], f[2]-d[2]])
+        normal_def = np.cross(de,df)
+        fg = np.array([g[0]-f[0], g[1]-f[1], g[2]-f[2]])
+        fh = np.array([h[0]-f[0], h[1]-f[1], h[2]-f[2]])
+        normal_fgh = np.cross(fg,fh)
+        gh = np.array([h[0]-g[0], h[1]-g[1], h[2]-g[2]])
+        gi = np.array([i[0]-g[0], i[1]-g[1], i[2]-g[2]])
+        normal_ghi = np.cross(gh,gi)
+        ij = np.array([j[0]-i[0], j[1]-i[1], j[2]-i[2]])
+        ik = np.array([k[0]-i[0], k[1]-i[1], k[2]-i[2]])
+        normal_ijk = np.cross(ij,ik)
+        jk = np.array([k[0]-j[0], k[1]-j[1], k[2]-j[2]])
+        jl = np.array([l[0]-j[0], l[1]-j[1], l[2]-j[2]])
+        normal_jkl = np.cross(jk,jl)
+
+        cos_theta_4 = np.dot(normal_cde, normal_def) / (np.linalg.norm(normal_cde) * np.linalg.norm(normal_def))
+        theta_test_4 = np.arccos(cos_theta_4)
+
+        cos_theta_5 = np.dot(normal_fgh, normal_ghi) / (np.linalg.norm(normal_fgh) * np.linalg.norm(normal_ghi))
+        theta_test_5 = np.arccos(cos_theta_5)
+
+        cos_theta_6 = np.dot(normal_ijk, normal_jkl) / (np.linalg.norm(normal_ijk) * np.linalg.norm(normal_jkl))
+        theta_test_6 = np.arccos(cos_theta_6)
+
+        for v in self.vectors_phi[0:10]:
+            print(v)
+        print("Normal_cde: {}".format(normal_cde))
+        print("Normal_def: {}".format(normal_def))
+        print("Normal_fgh: {}".format(normal_fgh))
+        print("Normal_ghi: {}".format(normal_ghi))
+        print("Normal_ijk: {}".format(normal_ijk))
+        print("Normal_jkl: {}".format(normal_jkl))
+
+        for p in self.phi_angle[0:3]:
+            print(p)
+        print("Theta_4: {}".format(theta_test_4))
+        print("Theta_5: {}".format(theta_test_5))
+        print("Theta_6: {}".format(theta_test_6))
+
+        elements_2 = []
+        for i in self.vectors_phi[2]:
+            elements_2.append(i)
+        for j in range(len(elements_2)):
+            self.assertAlmostEqual(elements_2[j],normal_cde[j])
+        elements_3 = []
+        for i in self.vectors_phi[3]:
+            elements_3.append(i)
+        for j in range(len(elements_3)):
+            self.assertAlmostEqual(elements_3[j],normal_def[j])
+        elements_5 = []
+        for i in self.vectors_phi[5]:
+            elements_5.append(i)
+        for j in range(len(elements_5)):
+            self.assertAlmostEqual(elements_5[j],normal_fgh[j])
+        elements_6 = []
+        for i in self.vectors_phi[6]:
+            elements_6.append(i)
+        for j in range(len(elements_6)):
+            self.assertAlmostEqual(elements_6[j],normal_ghi[j])
+        elements_8 = []
+        for i in self.vectors_phi[8]:
+            elements_8.append(i)
+        for j in range(len(elements_8)):
+            self.assertAlmostEqual(elements_8[j],normal_ijk[j])
+        elements_9 = []
+        for i in self.vectors_phi[9]:
+            elements_9.append(i)
+        for j in range(len(elements_9)):
+            self.assertAlmostEqual(elements_9[j],normal_jkl[j])
+
+        self.assertAlmostEqual(self.phi_angle[0], theta_test_4)
+        self.assertAlmostEqual(self.phi_angle[1], theta_test_5)
+        self.assertAlmostEqual(self.phi_angle[2], theta_test_6)
+
+
 
 def main():
     tester = TestRamachandran()
     tester.test_001_vector_norm_dihedral()
     tester.test_002_calculate_psi()
-
+    tester.test_003_calculate_phi()
 
 if __name__ == '__main__':
     main()
